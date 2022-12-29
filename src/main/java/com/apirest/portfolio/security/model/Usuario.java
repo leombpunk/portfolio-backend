f9ab78ca2ab75db57ebeb0160bf89deecee0b388
leombpunk <leomb_punk@hotmail.com>
2022-12-29 13:29:25 -0300

    Update Usuario.java

--- src/main/java/com/apirest/portfolio/security/model/Usuario.java
+++ src/main/java/com/apirest/portfolio/security/model/Usuario.java
@@ -39,7 +39,7 @@ public class Usuario {
     private String usuario;
     
     @NotBlank(message = "La contraseña no puede ser vacia")
-    @Size(min=8, max=255, message = "Entre 8 a 16 caracteres de lonqitud")
+    @Size(min=6, max=255, message = "Entre 6 a 16 caracteres de lonqitud")
     @Column(name="contrasena")
     private String contrasena;
     
